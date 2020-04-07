da902ad9646bf082e27ce348596d78bf04c35326
Rwanda S <rwanda.ace@gmail.com>
2020-04-07 20:31:49 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,21 @@
+class Post
+  @@all = []
+  
+  attr_accessor :title, :author
+  
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+  def author_name
+   if self.author == nil
+     return nil
+     else
+       self.author.name
+     end
+  end
+  def self.all
+    @@all
+  end
+  
+end
